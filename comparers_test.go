519e6f470f4076ea9bc3a1512ee7629989852379
Caybo Kotzé <kotzecabo@gmail.com>
2021-08-24 09:01:13 +0200

    :sparkles: add more tests

--- comparers_test.go
+++ comparers_test.go
@@ -59,7 +59,7 @@ func Test_Comparer_StringComparedToBoolean_ShouldBeFalse(t *testing.T) {
 
 func Test_Comparer_StructShouldEqualStruct_ShouldBeTrue(t *testing.T) {
 	// arrange
-	var person = test {
+	var person = person{
 		name: "Bob",
 		surname: "Williams",
 		age: 24,
@@ -76,18 +76,18 @@ func Test_Comparer_StructShouldEqualStruct_ShouldBeTrue(t *testing.T) {
 
 func Test_Comparer_StructShouldNotEqualDifferentStruct_ShouldBeFalse(t *testing.T) {
 	// arrange
-	var person = test {
+	var bob1 = person{
 		name: "Bob",
 		surname: "Williams",
 		age: 24,
 	}
-	var person2 = test {
+	var bob2 = person{
 		name: "Bob",
 		surname: "Williams",
 		age: 24,
 	}
 	// act
-	var expected = compare(person).to(person2)
+	var expected = compare(bob1).to(bob2)
 	// assert
 	if expected == false {
 		t.Log("A type should be equal to the same type")
@@ -95,7 +95,50 @@ func Test_Comparer_StructShouldNotEqualDifferentStruct_ShouldBeFalse(t *testing.
 	}
 }
 
-type test struct {
+func Test_Comparer_StructComparedToDifferentValues_ShouldBeFalse(t *testing.T) {
+	var person1 = person{
+		name: "Bob",
+		surname: "Seebert",
+		age: 54,
+	}
+	var person2 = person{
+		name: "John",
+		surname: "Mellow",
+		age: 39,
+	}
+	// act
+	// arrange
+	if compare(person1).to(person2) {
+		t.Log("A struct should not be equal to a different struct with different values")
+		t.Fail()
+	}
+}
+
+func Test_Comparer_StructEqualStruct_ShouldNotBeEqual(t *testing.T) {
+	// arrange
+	var bob1 = person{
+		name:    "Bob",
+		surname: "Williams",
+		age:     24,
+	}
+	var bob2 = person{
+		name: "Bob",
+		surname: "Williams",
+		age: 24,
+	}
+	// act
+	// assert
+	if !compare(bob1).to(bob2) {
+		t.Log("Compare should match the same structs")
+		t.Fail()
+	}
+	if compareRef(bob1).to(bob2) {
+		t.Log("2 structs should not be equal via compare ref")
+		t.Fail()
+	}
+}
+
+type person struct {
 	name string
 	surname string
 	age int
